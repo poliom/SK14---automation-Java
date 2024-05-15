993bc3fd523b38c000eeafaf67c7dded1ed4c1c7
Vidko Videv <vidko33@live.com>
2024-05-15 21:42:47 +0300

    Adding more console lines

--- SK14 Automation Java/src/Lecture3/GitWork.java
+++ SK14 Automation Java/src/Lecture3/GitWork.java
@@ -3,5 +3,7 @@ package Lecture3;
 public class GitWork {
     public static void main(String[] args) {
         System.out.println("Hi there");
+        System.out.println("Hi, how are you?");
+        System.out.println("Fine");
     }
 }
