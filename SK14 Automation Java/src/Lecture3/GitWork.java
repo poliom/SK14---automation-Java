3b2fcda212fa7966b40f8ac83cd21406c006a6a4
Ivan-Hristov-Ivanov <ivan.04.ivanov@gmail.com>
2024-05-15 21:38:10 +0300

    First Update

--- SK14 Automation Java/src/Lecture3/GitWork.java
+++ SK14 Automation Java/src/Lecture3/GitWork.java
@@ -3,5 +3,6 @@ package Lecture3;
 public class GitWork {
     public static void main(String[] args) {
         System.out.println("Hi there");
+        System.out.println("Update is here!");
     }
-}
+    }
