f6ad90fec3c4cde639a4c5f34a31757079dc9311
tsvetavasileva <tsveta.vasileva@abv.bg>
2024-05-20 01:24:03 +0300

    Excercise2 - p.1

---
+++ SK14 Automation Java/src/Lecture2/homework2.java
@@ -0,0 +1,11 @@
+package Lecture2;
+public class homework2 {
+    // Print the perimeter of a triangle by given sides. Use formula to calculate it.
+    public static void main(String [] args) {
+        String firstName = "Tsveta";
+        String middleName = "Valentinova";
+        String lastName = "Vasileva";
+
+        System.out.println(firstName + " " + middleName + " " + lastName);
+    }
+}
