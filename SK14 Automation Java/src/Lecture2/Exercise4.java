5acefaa3b50e0484523a9233b6142e64673fdb14
Vidko Videv <vidko33@live.com>
2024-05-20 19:52:32 +0300

    Add new line in exercise4

--- SK14 Automation Java/src/Lecture2/Exercise4.java
+++ SK14 Automation Java/src/Lecture2/Exercise4.java
@@ -3,6 +3,7 @@ package Lecture2;
 public class Exercise4 {
     public static void main(String[] args) {
         char asterisk = '*';
+        
         System.out.println("     " + asterisk);
         System.out.println("    " + asterisk + " " + asterisk);
         System.out.println("   " + asterisk + " " + asterisk + " " + asterisk);
