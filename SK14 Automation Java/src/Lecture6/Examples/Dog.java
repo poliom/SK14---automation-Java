bf0fffad33d41bf632f17fc8225e09dacae7a8b6
Vidko Videv <vidko33@live.com>
2024-05-27 18:53:40 +0300

    Lecture6 examples and exercises from lectures 4 and 5

---
+++ SK14 Automation Java/src/Lecture6/Examples/Dog.java
@@ -0,0 +1,9 @@
+package Lecture6.Examples;
+
+public class Dog extends Animal {
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("I am a dog.");
+    }
+}
