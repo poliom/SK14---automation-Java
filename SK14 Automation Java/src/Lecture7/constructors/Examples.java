77a8b3718ce1bf2cbc433341b71d382581ae9776
Vidko Videv <vidko33@live.com>
2024-05-29 18:52:11 +0300

    Add examples for lecture 7 and Exercises from lecture 6

---
+++ SK14 Automation Java/src/Lecture7/constructors/Examples.java
@@ -0,0 +1,11 @@
+package Lecture7.constructors;
+
+public class Examples {
+    public static void main(String[] args) {
+    }
+
+    static Employee createEmployee() {
+        Employee employee = new Employee();
+        return employee;
+    }
+}
