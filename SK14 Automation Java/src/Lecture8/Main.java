22a736dbcbbbe0aeb6b8f17f4c76ec2b2ac343b6
tsvetavasileva <tsveta.vasileva@abv.bg>
2024-06-09 19:53:45 +0300

    Lecture 8 - added excersices

--- SK14 Automation Java/src/Lecture8/Main.java
+++ SK14 Automation Java/src/Lecture8/Main.java
@@ -2,13 +2,15 @@ package Lecture8;
 
 public class Main {
     public static void main(String[] args) {
+
     Circle s1 = new Circle();
     System.out.println(s1.getArea(3.0));
     System.out.println(s1);
 
     Shape s2 = new Circle();
-System.out.println(s2.getArea(3.0));
-        System.out.println(s2);
+    System.out.println(s2.getArea(3.0));
+    System.out.println(s2);
+
     Circle s3 = (Circle) s2;
 System.out .println(s3);
         System.out.println(s2.getPerimeter(3.0));
