1952b7ce355a9b0a3224a12315ed82dc9a0bc699
tsvetavasileva <tsveta.vasileva@abv.bg>
2024-06-15 23:21:44 +0300

    Excercise10 - homework success

--- src/main/java/Lecture8/Employee.java
+++ src/main/java/Lecture8/Employee.java
@@ -1,41 +1,41 @@
+public static class Employee {
+                final private static int id = 1234;
+                final private static String firstName = "Ana";
+                final private static String lastName = "Nikolova";
+                private static double salaryPerMonth = 6000;
 
-        public class Employee {
-            final private static int id = 1234;
-            final private static String firstName = "Ana";
-            final private static String lastName = "Nikolova";
-            private static double salaryPerMonth = 6000;
+                public Employee (int id, String firstName, String lastName, double salaryPerMonth) {
+                    this.salaryPerMonth = salaryPerMonth;
 
-            public Employee (int id, String firstName, String lastName, double salaryPerMonth) {
-                this.salaryPerMonth = salaryPerMonth;
-
-                Employee employee = new Employee(1234, "Petya", "Pavlova", 7000);
+                    Employee employee = new Employee(1234, "Petya", "Pavlova", 7000);
+                }
             }
-        }
 
-        static void getFirstNameLastName () {
-            System.out.println(Employee.firstName + " " + Employee.lastName);
+            static void getFirstNameLastName () {
+                System.out.println(Employee.firstName + " " + Employee.lastName);
 
-        }
+            }
 
-        public static double getAnnualsalary () {
-            return Employee.salaryPerMonth * 12;
-        }
+            public static double getAnnualsalary () {
+                return Employee.salaryPerMonth * 12;
+            }
 
-        public void main(String[] args) {
-            getFirstNameLastName();
-            getAnnualsalary();
-            raiseSalary();
-        }
+            public void main (String[] args) {
+                getFirstNameLastName();
+                getAnnualsalary();
+                raiseSalary();
+            }
 
-        private void raiseSalary () {
-            double percentage = 0x3;
-            double salaryPerMonth = 7000;
-            double raiseAmount = salaryPerMonth * percentage / 100;
-            double raisePerMonth = raiseAmount;
-            double monhtlyRaise = raisePerMonth + salaryPerMonth;
-            double salaryPerYear = salaryPerMonth * 12;
-            System.out.println(salaryPerYear);
-            System.out.println(monhtlyRaise);
-        }
+            private void raiseSalary () {
+                double percentage = 0x3;
+                double salaryPerMonth = 7000;
+                double raiseAmount = salaryPerMonth * percentage / 100;
+                double raisePerMonth = raiseAmount;
+                double monhtlyRaise = raisePerMonth + salaryPerMonth;
+                double salaryPerYear = salaryPerMonth * 12;
+                System.out.println(salaryPerYear);
+                System.out.println(monhtlyRaise);
+            }
+
 
 
--- src/test/java/Lecture10/examples/DataProviderTests.java
+++ src/test/java/Lecture10/examples/DataProviderTests.java
@@ -8,11 +8,11 @@ public class DataProviderTests {
 
     @DataProvider(name = "generateValidTestData")
     public Object[][] generateValidTestData() {
-        return new Object[][] { {1, 2}, {2, 3} };
+        return new Object[][] { {1, 2, 3}, {2, 3 ,5} };
     }
     @DataProvider(name = "generateInvalidTestData")
     public Object[][] generateInvalidTestData() {
-        return new Object[][]{ {3, 4}, {4, 5} };
+        return new Object[][]{ {3, 4, 8}, {4, 5, 10} };
     }
 
     @Test(dataProvider = "generateValidTestData", groups = "validTests")
